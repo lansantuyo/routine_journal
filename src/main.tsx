16746cd16bbeeca8dfeb304e0c509841051037c0
Lance Santuyo <oyutnasecnal@gmail.com>
2024-03-05 22:18:46 +0800

    Updated to use React 18 code

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-// import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  //Component Tree of Project
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(<App />);
