814a4c04de905f4648e5cc60929d02461b1ada1f
Lance Santuyo <oyutnasecnal@gmail.com>
2024-03-05 23:19:44 +0800

    Updated naming conventions to better fit the app

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
 
-// Define an interface for the note and the component props
-interface Note {
+interface Entry {
     id: string;
     body: string;
 }
 
 interface SidebarProps {
-    entries: Note[];
-    currentEntry: Note;
+    entries: Entry[];
+    currentEntry: Entry;
     setCurrentEntryId: (id: string) => void;
     newEntry: () => void;
     deleteEntry: (id: string) => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = (props) => {
-    const noteElements = props.entries.map((note, index) => (
-        <div key={note.id}>
+    const entryElements = props.entries.map((entry, index) => (
+        <div key={entry.id}>
             <div
-                className={`title ${note.id === props.currentEntry.id ? "selected-note" : ""}`}
-                onClick={() => props.setCurrentEntryId(note.id)}
+                className={`title ${entry.id === props.currentEntry.id ? "selected-entry" : ""}`}
+                onClick={() => props.setCurrentEntryId(entry.id)}
             >
-                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+                <h4 className="text-snippet">{entry.body.split("\n")[0]}</h4>
                 <button
                     className="delete-btn"
                     onClick={(e) => {
                         e.stopPropagation(); // Prevent onClick from triggering when the button is clicked
-                        props.deleteEntry(note.id);
+                        props.deleteEntry(entry.id);
                     }}
                 >
                     <i className="gg-trash trash-icon"></i>
@@ -38,10 +37,10 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
     return (
         <section className="pane sidebar">
             <div className="sidebar--header">
-                <h3>Notes</h3>
+                <h3>Entries</h3>
                 <button className="new-entry" onClick={props.newEntry}>+</button>
             </div>
-            {noteElements}
+            {entryElements}
         </section>
     );
 };
