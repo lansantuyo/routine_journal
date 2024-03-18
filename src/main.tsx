a168dfe4f2c38ab9ea7f24010bed6b67d0de842f
Lance Santuyo <oyutnasecnal@gmail.com>
2024-03-19 04:46:28 +0800

    Updated the router to instead use BrowserRouter Fixed the structure of the rendered HTML in ReactDOM

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-// import './index.css'
+import './index.css'
+import {createTheme, MantineProvider} from "@mantine/core";
+import {BrowserRouter} from "react-router-dom";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
+const theme = createTheme({
 
-//Routing Sauce: https://www.youtube.com/watch?v=oTIJunBa6MA
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element:  <HomePage />,
-    errorElement: <NotFoundPage />,
-  },
-  // ROUTING TEMPLATE:
-  // {
-  //   path: '/journal',
-  //   element: <JournalPage />,
-  // },
-  // DONT FORGET TO IMPORT THE .tsx FILE
-]);
+});
 
-
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  //Component Tree of Project
-  <React.StrictMode>
-    <RouterProvider router={router} />
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <MantineProvider theme={theme} defaultColorScheme='dark'>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </MantineProvider>
+    </React.StrictMode>,
 )
