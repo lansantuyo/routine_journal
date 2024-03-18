16bb1464dc2e453fb7295af146c42f9c24da11cc
Lance Santuyo <oyutnasecnal@gmail.com>
2024-03-19 04:49:46 +0800

    Updated the root App to handle Mantine components Added the Header, Navbar, RouterSwitcher, and Footer components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import Message from './Message';
-import ListGroup from './components/ListGroup';
+import './App.css'
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import RouterSwitcher from "./components/RouterSwitcher";
 
-function App(){
+import '@mantine/core/styles.css';
+
+import {
+  AppShell,
+} from '@mantine/core';
+
+import {useDisclosure} from "@mantine/hooks";
+
+function App() {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
-      <><div><Message /></div><div><ListGroup /></div></>
-  )
-
+      <AppShell
+          header = {{ height: 60  }}
+          navbar = {{ width: 300, breakpoint: 'sm', collapsed: {mobile: !mobileOpened, desktop: !desktopOpened}}}
+          padding = 'md'
+      >
+        <Header toggleDesktop={toggleDesktop} toggleMobile={toggleMobile} desktopOpened={desktopOpened} mobileOpened={mobileOpened} />
+        <Navbar />
+        <AppShell.Main>
+          <RouterSwitcher />
+        </AppShell.Main>
+        <AppShell.Footer>
+          Footer here
+        </AppShell.Footer>
+      </AppShell>
+  );
 }
 
-export default App;
+export default App
