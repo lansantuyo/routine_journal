46f86d4d590ca898171b01fc680841d91aa79649
csci40 finalproject <christianolivares13@gmail.com>
2024-03-18 11:56:05 +0800

    implemented routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,31 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 // import './index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+
+//Routing Sauce: https://www.youtube.com/watch?v=oTIJunBa6MA
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:  <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  // ROUTING TEMPLATE:
+  // {
+  //   path: '/journal',
+  //   element: <JournalPage />,
+  // },
+  // DONT FORGET TO IMPORT THE .tsx FILE
+]);
+
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   //Component Tree of Project
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+
+interface HomePageProps {
+    
+}
+ 
+const HomePage: FunctionComponent<HomePageProps> = () => {
+    return (  
+        <div>
+            <p>This is the HomePage.</p>
+        </div>
+    )
+}
+ 
+export default HomePage;
