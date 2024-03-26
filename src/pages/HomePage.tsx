6e3cef0e1cc0615a3971057c29f54789adf8f6f4
csci40 finalproject <christianolivares13@gmail.com>
2024-03-26 19:24:29 +0800

    resized calendar header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 //Source: https://mantine.dev/app-shell/?e=CollapseDesktop&s=code
 import { useState } from 'react';
-import { AppShell, Burger, Container, Space, Group, Stack, Collapse } from '@mantine/core';
+import { AppShell, Burger, Container, Space, Group, Stack, Collapse, Select } from '@mantine/core';
 import { DatePicker, DatePickerProps, Day  } from '@mantine/dates';
 import { useDisclosure } from '@mantine/hooks';
 import { Link } from 'react-router-dom';
@@ -50,24 +50,42 @@ export default function HomePage() {
         </AppShell.Aside>
 
         <AppShell.Main>
-            <div style={{position: 'fixed', top: '100px',left: '200px'}}>
+            <div style={{position: 'fixed', top: '70px', left: '80px'}}>
                     <DatePicker 
-                        allowDeselect
+                        allowDeselect = {true}
                         value={value}
                         onChange={setValue}
+                        hasNextLevel = {false}
                         maxLevel="month"
                         size='xl'
                         monthLabelFormat="MMMM"
-                        // bg={'cyan'}
                         styles={{
                             day: {
-                                color: '#543f3f',
+                                // color: '#543f3f',
                                 backgroundColor: '#ead8c2',
-                            }
+                                borderRadius: '4%',
+                                padding: '5px',
+                                margin: '10px',
+                                fontFamily: 'Inter',
+                                fontWeight: '500',
+                                fontSize: '32px',
+                                height: '70px',
+                                width: '70px',
+                            },
+                            calendarHeaderLevel: {
+                                fontFamily: 'Inter',
+                                fontWeight: '300',
+                                fontSize: '48px',
+                                textAlign: 'center',
+                            },
+                            calendarHeader: {
+                                width: '400px',
+                                transform: 'translateX(32%)',
+                            },
                         }}
                     />
             </div>
-            <div style={{position: 'fixed', top: '100px', left: '800px'}}>
+            <div style={{position: 'fixed', top: '70px', left: '800px'}}>
                 <h2>Journal of</h2>
                 <h1>USERNAME :D</h1>
             </div>
