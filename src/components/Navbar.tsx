b400bd918d29c069c77982377db659a17097a8a2
chuasharmaine <sharmaine.chua@obf.ateneo.edu>
2024-03-30 14:34:31 +0800

    tried to fix routing of activity detail but failed. TODO: fix routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import {
     AppShell,
     NavLink
 } from '@mantine/core';
-
 import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
@@ -25,9 +24,15 @@ const Navbar = () => {
                 onClick={() => navigate('/JournalEntryPage')}
                 style={{ margin: '5px' }}
             />
+            <NavLink
+                label="Activity Detail"
+                onClick={() => navigate('/Detail')} 
+                style={{ margin: '5px' }}
+            />
             <NavLink onClick={() => navigate('/Homepage')} style={{ margin: '5px' }}>Homepage</NavLink>
             <NavLink onClick={() => navigate('/Activities')} style={{ margin: '5px' }}>Activities</NavLink>
             <NavLink onClick={() => navigate('/Journal')} style={{ margin: '5px' }}>Journal</NavLink>
+            <NavLink onClick={() => navigate('/Detail')} style={{ margin: '5px' }}>Activity Detail</NavLink>
         </AppShell.Navbar>
     );
 }
--- src/components/RouterSwitcher.tsx
+++ src/components/RouterSwitcher.tsx
@@ -3,8 +3,9 @@ import NotFound from './NotFound';
 import TextComponent from './Text';
 import ButtonComponent from './Button';
 import JournalEntryPage from '../pages/JournalEntryPage';
+import ActivityDetailPage from '../pages/ActivityDetailPage';
 
-const RouteSwitcher = () => {
+const RouterSwitcher = () => {
     return (
         <Routes>
             {/*Replace path with path, element with component of that page*/}
@@ -12,8 +13,9 @@ const RouteSwitcher = () => {
             <Route path="/Journal" element={<JournalEntryPage />} />
             <Route path="/Activities" element={<ButtonComponent />} />
             <Route path="/Homepage" element={<ButtonComponent />} />
+            <Route path="/Detail" element={<ActivityDetailPage />} /> 
         </Routes>
     );
 };
 
-export default RouteSwitcher;
+export default RouterSwitcher;
