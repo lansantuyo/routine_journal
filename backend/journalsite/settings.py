73246d1f90b2b0b492d3496814b2712de7bac1de
Lance Santuyo <oyutnasecnal@gmail.com>
2024-04-21 16:01:17 +0800

    Restructured project by moving frontend and backend files into their respective directories

--- backend/journalsite/settings.py
+++ backend/journalsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'corsheaders',
     'rest_framework',
+
     'activity',
     'journal_entry',
 ]
@@ -121,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR.parent, 'frontend/dist'),  # Path to the built React app
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
