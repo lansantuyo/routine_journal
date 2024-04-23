ce51b079a13438e94be27e571bc8cd4b9d135040
chuasharmaine <sharmaine.chua@obf.ateneo.edu>
2024-04-23 21:49:01 +0800

    fixed bg color and font color of light mode and dark mode

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import '../styles/App.css'
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
@@ -7,22 +8,37 @@ import '@mantine/core/styles.css';
 import '@mantine/tiptap/styles.css';
 
 import {
-  AppShell,
+  AppShell, useComputedColorScheme, useMantineColorScheme
 } from '@mantine/core';
 
-import {useDisclosure} from "@mantine/hooks";
+import { useDisclosure, } from "@mantine/hooks";
 
 function App() {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+  const computedColorScheme = useComputedColorScheme('light');
+  const { setColorScheme } = useMantineColorScheme();
+
+  const toggleColorScheme = () => {
+      setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light');
+  }
 
   return (
       <AppShell
           header = {{ height: 60  }}
           navbar = {{ width: 300, breakpoint: 'sm', collapsed: {mobile: !mobileOpened, desktop: !desktopOpened}}}
           padding = 'md'
+          style = {{ 
+            backgroundColor: computedColorScheme === 'dark' ? '#543F3F' : '#EAD8C2', 
+            color: computedColorScheme === 'dark' ? '#EAD8C2' : '#543F3F'
+          }}
       >
-        <Header toggleDesktop={toggleDesktop} toggleMobile={toggleMobile} desktopOpened={desktopOpened} mobileOpened={mobileOpened} />
+        <Header 
+          toggleDesktop={toggleDesktop} 
+          toggleMobile={toggleMobile} 
+          desktopOpened={desktopOpened} 
+          mobileOpened={mobileOpened} 
+        />
         <Navbar />
         <AppShell.Main>
           <RouterSwitcher />
