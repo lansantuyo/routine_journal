465cdabf56cc2c8d2e98e69cbeafcdab2cec1e4c
Lance Santuyo <oyutnasecnal@gmail.com>
2024-05-03 23:02:45 +0800

    Fixed the routing for root page to properly handle child routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-              path="/"
+              path="/*"
               element={
                 <ProtectedRoute>
                   <Home />
